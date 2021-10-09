02b969db5a283c3f0a297942e1276fc695deb35e
LukaszZietek <lukaszzietekk@gmail.com>
2021-10-09 11:17:53 +0200

    Add form validation to PasswordChanger

--- src/components/Content/subcomponents/PasswordChanger/PasswordChanger.jsx
+++ src/components/Content/subcomponents/PasswordChanger/PasswordChanger.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './PasswordChanger.css';
+
+import CreateSimpleReactValidator from '../../SimpleValidatorTranslation';
 
 const PasswordChanger = () => {
     const [oldPassword, setOldPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [repeatedPassword, setRepeatedPassword] = useState('');
+    const [, forceUpdate] = useState();
+    const simpleValidator = useRef(CreateSimpleReactValidator(forceUpdate));
+    simpleValidator.current.messages['in'] = 'Hasla powinny byc jednakowe';
 
     const handleOnOldPasswordChange = e => setOldPassword(e.target.value);
     const handleOnNewPasswordChange = e => setNewPassword(e.target.value);
@@ -13,7 +18,20 @@ const PasswordChanger = () => {
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
-        alert('Zmieniono hasło');
+        const formValid = simpleValidator.current.allValid();
+        if (!formValid) {
+            simpleValidator.current.showMessages();
+            forceUpdate(1);
+        } else {
+            alert('Hasło zmienione');
+            resetInputStates();
+        }
+    }
+
+    const resetInputStates = () => {
+        setOldPassword('');
+        setNewPassword('');
+        setRepeatedPassword('');
     }
 
     return (
@@ -26,6 +44,7 @@ const PasswordChanger = () => {
                         <br/>
                         <input className="password-changer-input" type="password" value={oldPassword}
                             onChange={handleOnOldPasswordChange}/>
+                        <p className="validator-message">{simpleValidator.current.message('poprzednie hasło', oldPassword, 'required|min:6,string|max:20,string|alpha_num_dash')}</p>
                     </label>
                 </div>
                 <div className="password-row">
@@ -34,6 +53,7 @@ const PasswordChanger = () => {
                         <br/>
                         <input className="password-changer-input" type="password" value={newPassword}
                             onChange={handleOnNewPasswordChange}/>
+                        <p className="validator-message">{simpleValidator.current.message('nowe hasło', newPassword, 'required|min:6,string|max:20,string|alpha_num_dash')}</p>
                     </label>
                 </div>
                 <div className="password-row">
@@ -42,6 +62,7 @@ const PasswordChanger = () => {
                         <br/>
                         <input className="password-changer-input" type="password" value={repeatedPassword}
                             onChange={handleOnRepeatedPasswordChange}/>
+                        <p className="validator-message">{simpleValidator.current.message('powtórz hasło', repeatedPassword, `required|min:6,string|max:20,string|alpha_num_dash|in:${newPassword}`)}</p>
                     </label>
                 </div>
                 <div className="password-row">
