066998c8aa89b47d1d5ba75663b86ce31d9d6536
LukaszZietek <lukaszzietekk@gmail.com>
2021-11-01 18:25:30 +0100

    Style Contact component

--- src/components/Content/subcomponents/Contact/Contact.jsx
+++ src/components/Content/subcomponents/Contact/Contact.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 
 import './Contact.css';
 
+import logo from '../../../NavBar/logo.png';
+
 
 const Contact = () => {
     return (
         <div className="contact-container center-div">
-            <h2>FITNESS WEBSITE</h2>
-            <h1>Kontakt</h1>
-            <h3>Adres: </h3> ul.Matejki xx/7 67-100 Nowa Sól
-            <h3>Tel:</h3> +48 888 999 999
-            <h3>Email: </h3> 249400@student.pwr.edu.pl
-            <h3>Fax: </h3> 888 999 991
-            <h3>Nawiązanie współpracy:</h3> lukaszzietekk@gmail.com
+            <div className="inner-contact-div">
+                <h2>FITNESS WEBSITE</h2>
+                <img src={logo} alt="logo" />
+                <h1>Kontakt</h1>
+                <h3>Adres: </h3> ul.Matejki xx/7 67-100 Nowa Sól
+                <h3>Tel:</h3> +48 888 999 999
+                <h3>Email: </h3> 249400@student.pwr.edu.pl
+                <h3>Fax: </h3> 888 999 991
+                <h3>Nawiązanie współpracy:</h3> lukaszzietekk@gmail.com
+            </div>
         </div>
     );
 };
